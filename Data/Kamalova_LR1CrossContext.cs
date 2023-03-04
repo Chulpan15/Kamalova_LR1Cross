53e2535975b33822d084ac2c49ea2c82da631249
Chulpan15 <ch_kamalova@mail.ru>
2023-03-04 11:40:31 +0300

    Добавьте файлы проекта.

---
+++ Data/Kamalova_LR1CrossContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Kamalova_LR1Cross.Models;
+
+namespace Kamalova_LR1Cross.Data
+{
+    public class Kamalova_LR1CrossContext : DbContext
+    {
+        public Kamalova_LR1CrossContext (DbContextOptions<Kamalova_LR1CrossContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Kamalova_LR1Cross.Models.Movie> Movie { get; set; } = default!;
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Kamalova_LR1Cross.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
